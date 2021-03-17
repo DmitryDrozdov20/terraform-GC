b543c25c801cc154b40a9f85ae15ba9475345179
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-18 02:03:37 +0300

    out

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "app_external-ip" {
-  value="${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+// A variable for extracting the external IP address of the instance
+output "ip" {
+ value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
