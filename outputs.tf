6b52d6e2473f575984f9fbb11c0b5df67b81d6a4
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-19 02:06:07 +0300

    ghhg

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 // A variable for extracting the external IP address of the instance
 output "ip" {
- value = google_compute_instance.app.network_interface.0.access_config.0.nat_ip
+ value = google_compute_instance.stage.network_interface.0.access_config.0.nat_ip
 }
