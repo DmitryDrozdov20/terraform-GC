7b8706b7e7ffcb8fbddfeb7a9dcb366924b50953
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-21 23:23:42 +0300

    add 2 instance

--- main.tf
+++ main.tf
@@ -36,6 +36,40 @@ resource "google_compute_instance" "stage" {
   allow_stopping_for_update = true
 
   boot_disk {
+    auto_delete = "yes"
+    initialize_params {
+      size = "10"
+      type = "pd-balanced" // Available options: pd-standard, pd-balanced, pd-ssd
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  // Startup script - update, install python3-pip (for Ansible) 
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install python3-pip -y"
+
+  network_interface {
+  network = "default"
+
+  access_config {
+    nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+
+  #metadata = {
+  #  ssh-keys = "root:${file("/root/.ssh/id_rsa.pub")}" // Copy ssh public key
+  #}
+
+  resource "google_compute_instance" "prod" {
+  name         = "prod"
+  machine_type = "e2-small" // 2vCPU, 2GB RAM
+  #machine_type = "e2-medium" // 2vCPU, 4GB RAM
+  #machine_type = "custom-6-20480" // 6vCPU, 20GB RAM
+  #machine_type = "custom-2-15360-ext" // 2vCPU, 15GB RAM
+
+  #tags = ["terraform", "template"]
+  allow_stopping_for_update = true
+
+  boot_disk {
     initialize_params {
       size = "10"
       type = "pd-balanced" // Available options: pd-standard, pd-balanced, pd-ssd
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 // A variable for extracting the external IP address of the instance
 output "ip" {
  value = google_compute_instance.stage.network_interface.0.access_config.0.nat_ip
+ value = google_compute_instance.prod.network_interface.0.access_config.0.nat_ip
 }
