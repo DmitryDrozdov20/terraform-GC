be539f31697b70c6141c2bce8d16ba6f8c1e5242
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-23 00:47:42 +0300

    gjj

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
-output "ip-stage" {
+output "ip-stage[count.index]" {
     value = google_compute_instance.default[count.index].network_interface.0.access_config.0.nat_ip
 }
 
