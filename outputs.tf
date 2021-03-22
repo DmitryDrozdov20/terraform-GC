03f9aa81bb2ce88141cd11a2579d2580319d3055
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-22 03:48:09 +0300

    333

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 // A variable for extracting the external IP address of the instance
-#output "ip-stage" {
-# value = google_compute_instance.default[1].network_interface.0.access_config.0.nat_ip
-#}
-#output "ip-prod" {
-# value = google_compute_instance.default[2].network_interface.0.access_config.0.nat_ip
-#}
+output "ip-stage" {
+ value = google_compute_instance.default[count.index].network_interface.0.access_config.0.nat_ip
+}
+output "ip-prod" {
+ value = google_compute_instance.default[count.index].network_interface.0.access_config.0.nat_ip
+}
