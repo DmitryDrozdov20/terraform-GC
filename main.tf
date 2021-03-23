9aa8e889e82a5a9cadeec0964e588b56216aa5e1
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-24 02:52:07 +0300

    4544

--- main.tf
+++ main.tf
@@ -57,9 +57,48 @@ resource "google_compute_instance" "vm_stage" {
 output "stage_ip" {
  value = google_compute_instance.vm_stage.network_interface.0.access_config.0.nat_ip
 }
+
+resource "google_compute_instance" "vm_prod" {
+  name = "prod"
+  # count = var.node_count
+  # name = element(tolist(var.instance_tags), count.index)
+  machine_type = var.machine_type // 2vCPU, 2GB RAM
+  
+  allow_stopping_for_update = true
+
+  boot_disk {
+    auto_delete = "true"
+    initialize_params {
+      size = "10"
+      type = "pd-balanced" // Available options: pd-standard, pd-balanced, pd-ssd
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  # Startup script - update, install python3-pip (for Ansible) 
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install python3-pip -y"
+
+  network_interface {
+  network = "default"
+
+  access_config {
+    nat_ip = google_compute_address.vm_prod_ip.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "root:${file("id_rsa.pub")}" // Copy ssh public key
+    }
+  }  
+
+# Static IP VM for Ansible
+output "prod_ip" {
+ value = google_compute_instance.vm_prod.network_interface.0.access_config.0.nat_ip
+}
+
 # Waiting_30s 
 resource  "time_sleep" "wait_30_seconds" {
-  depends_on = [google_compute_instance.vm_stage]
+  depends_on = [google_compute_instance.vm_prod]
 
   create_duration = "30s"
 }
@@ -74,7 +113,7 @@ resource "null_resource" "ansible_hosts_provisioner" {
 [stage] 
 $(terraform output stage_ip)
 [prod]
-$(terraform output stage_ip)
+$(terraform output prod_ip)
 EOF
       export ANSIBLE_HOST_KEY_CHECKING=False
     EOT
