fefb2ee52c3cbc842f7e66c411ef7f10c2a40db2
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-24 01:42:44 +0300

    nm

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 // A variable for extracting the external IP address of the instance
-output "ip2" {
- value = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
-}
+#output "ip2" {
+# value = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
+#}
 #output "ip-prod" {
 # value = google_compute_instance.default[1].network_interface.0.access_config.0.nat_ip
 #}
